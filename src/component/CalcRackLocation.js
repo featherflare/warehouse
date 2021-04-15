39394dad1c10b7fe9fc42acd45a250dfec62d43b
Sorawis Pim-aksorn <68042695+tipleaum@users.noreply.github.com>
2021-04-15 22:49:56 +0700

    fix slice location and current location

--- src/component/CalcRackLocation.js
+++ src/component/CalcRackLocation.js
@@ -6,25 +6,35 @@ sum row and shelf together.
 Then return array object.
 
 ----- data variable -----
-Recieve the data of Location in type of String(10).
+Recieve the data of Location in type of String(5).
+
+@ INPUT
+A0102:
+A - row
+01 - floorRack
+02 - column
 
 destination --> destination of pallet to pick up or put-away.
 current --> user location now.
 
-Updated 06-04-2021
+@ OUTPUT
+A-02: rackLocation
+B-01: curLocation
+
+Updated 15-04-2021
 */
 
 const CalcRackLocation = (destination, current) => {
     if (destination) {
-        var row = destination.slice(0, 6);
-        var floorRack = destination.slice(6, 8);
-        var shelf = destination.slice(8, 10);
+        var row = destination.slice(0, 1);
+        var floorRack = destination.slice(1, 3);
+        var shelf = destination.slice(3, 5);
         var rackLocation = `${row}-${shelf}`;
     }
     
     if (current) {
-        const currentLocation = `${current.slice(0, 6)}-${current.slice(8, 10)}`;
-        const currentFloorRack = current.slice(6, 8);
+        const currentLocation = `${current.slice(0, 1)}-${current.slice(3, 5)}`;
+        const currentFloorRack = current.slice(1, 3);
         const rack = [
             {
                 row: row,
@@ -34,7 +44,7 @@ const CalcRackLocation = (destination, current) => {
                 curLocation: currentLocation,
                 curFloorRack: currentFloorRack
             }
-        ]
+        ];
         return rack;
     } else {
         const rack = [
