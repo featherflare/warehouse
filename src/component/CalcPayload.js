c4158e2c05e269d96a2a35ef86dc450015c3b861
Sorawis Pim-aksorn <68042695+tipleaum@users.noreply.github.com>
2021-04-15 22:47:23 +0700

    fix receive payload

--- src/component/CalcPayload.js
+++ src/component/CalcPayload.js
@@ -8,13 +8,11 @@ Updated - 08/03/64 - Aum
 const CalcPayload = (dataFromServer) => {
     let payload = [];
 
-    // mode 0 select mode
-    if (dataFromServer.mode === 0) {
+    if (dataFromServer.mode === 0) { // select mode page
         payload = [dataFromServer];
-    } else if (dataFromServer.mode === 1) { // mode 1
-        // only have stage 3
-        if (dataFromServer.stage === 3) {
-            const [{ item_number, item_name }] = dataFromServer.data;
+    } else if (dataFromServer.mode === 2) { // mode 2: Put-away mode
+        if (dataFromServer.stage === 1) {
+            const [{ item_name, item_number, location }] = dataFromServer.data;
             payload = [
                 {
                     mode: dataFromServer.mode,
@@ -22,114 +20,89 @@ const CalcPayload = (dataFromServer) => {
                     is_notify: dataFromServer.is_notify,
                     status: dataFromServer.status,
                     error_type: dataFromServer.error_type,
+                    item_number: item_number,
                     item_name: item_name,
-                    item_number: item_number
+                    location: location
                 }
             ];
         } else {
             payload = [dataFromServer];
         }
-    } else if (dataFromServer.mode === 2) { // mode 2: Put-away mode
+    } else if (dataFromServer.mode === 3) { // pick up mode
         if (dataFromServer.stage === 0) {
-            const [{ location, item_number, item_name }] = dataFromServer.data;
+            const [{ order_number, pickup_id, pickup_type, item_name, location }] = dataFromServer.data;
             payload = [
                 {
                     mode: dataFromServer.mode,
                     stage: dataFromServer.stage,
                     is_notify: dataFromServer.is_notify,
                     status: dataFromServer.status,
-                    item_number: item_number,
+                    hardware_status: dataFromServer.hardware_status,
+                    total_pickup: dataFromServer.total_pickup,
+                    done_pickup: dataFromServer.done_pickup,
+                    order_number: order_number,
+                    pickup_id: pickup_id,
+                    pickup_type: pickup_type,
                     item_name: item_name,
                     location: location
                 }
             ];
-        } else if (dataFromServer.stage === 1) {
-            payload = [dataFromServer];
-        } else if (dataFromServer.stage === 2) {
-            payload = [dataFromServer];
-        }
-    } else if (dataFromServer.mode === 3) {
-        if (dataFromServer.stage === 0) {
-            const [{ order_number, pickup_id, pickup_type, item_name, location }] = dataFromServer.data;
-            if (pickup_type === 'FULL') {
-                payload = [
-                    {
-                        mode: dataFromServer.mode,
-                        stage: dataFromServer.stage,
-                        is_notify: dataFromServer.is_notify,
-                        status: dataFromServer.status,
-                        total_pickup: dataFromServer.total_pickup,
-                        done_pickup: dataFromServer.done_pickup,
-                        order_number: order_number,
-                        pickup_id: pickup_id,
-                        pickup_type: pickup_type,
-                        item_name: item_name,
-                        location: location
-                    }
-                ];
-            } else if (pickup_type === 'SHOPPING') {
-                const [{ amount }] = dataFromServer.data;
-                payload = [
-                    {
-                        mode: dataFromServer.mode,
-                        stage: dataFromServer.stage,
-                        is_notify: dataFromServer.is_notify,
-                        status: dataFromServer.status,
-                        total_pickup: dataFromServer.total_pickup,
-                        done_pickup: dataFromServer.done_pickup,
-                        order_number: order_number,
-                        pickup_id: pickup_id,
-                        pickup_type: pickup_type,
-                        item_name: item_name,
-                        location: location,
-                        amount: amount
-                    }
-                ];
-            }
         } else if (dataFromServer.stage === 4) {
             const [{ order_number, pickup_id, pickup_type, item_name, location }] = dataFromServer.data;
-            if (pickup_type === 'FULL') {
-                payload = [
-                    {
-                        mode: dataFromServer.mode,
-                        stage: dataFromServer.stage,
-                        is_notify: dataFromServer.is_notify,
-                        status: dataFromServer.status,
-                        total_pickup: dataFromServer.total_pickup,
-                        done_pickup: dataFromServer.done_pickup,
-                        order_number: order_number,
-                        pickup_id: pickup_id,
-                        pickup_type: pickup_type,
-                        item_name: item_name,
-                        location: location
-                    }
-                ];
-            } else if (pickup_type === 'SHOPPING') {
-                const [{ amount }] = dataFromServer.data;
-                payload = [
-                    {
-                        mode: dataFromServer.mode,
-                        stage: dataFromServer.stage,
-                        is_notify: dataFromServer.is_notify,
-                        status: dataFromServer.status,
-                        total_pickup: dataFromServer.total_pickup,
-                        done_pickup: dataFromServer.done_pickup,
-                        order_number: order_number,
-                        pickup_id: pickup_id,
-                        pickup_type: pickup_type,
-                        item_name: item_name,
-                        location: location,
-                        amount: amount
-                    }
-                ];
-            }
+            payload = [
+                {
+                    mode: dataFromServer.mode,
+                    stage: dataFromServer.stage,
+                    is_notify: dataFromServer.is_notify,
+                    status: dataFromServer.status,
+                    total_pickup: dataFromServer.total_pickup,
+                    done_pickup: dataFromServer.done_pickup,
+                    order_number: order_number,
+                    pickup_id: pickup_id,
+                    pickup_type: pickup_type,
+                    item_name: item_name,
+                    location: location
+                }
+            ];
         } else {
             payload = [dataFromServer];
         }
-    } else if (dataFromServer.mode === 4) {
-
+    } else if (dataFromServer.mode === 4) { // location transfer mode
+        if (dataFromServer.stage === 0) {
+            const [{ source, destination }] = dataFromServer.data;
+            payload = [
+                {
+                    mode: dataFromServer.mode,
+                    stage: dataFromServer.stage,
+                    is_notify: dataFromServer.is_notify,
+                    status: dataFromServer.status,
+                    hardware_status: dataFromServer.hardware_status,
+                    total_location_transfer: dataFromServer.total_location_transfer,
+                    done_location_transfer: dataFromServer.done_location_transfer,
+                    source: source,
+                    destination: destination
+                }
+            ];
+        } else if (dataFromServer.stage === 4) {
+            const [{ source, destination }] = dataFromServer.data;
+            payload = [
+                {
+                    mode: dataFromServer.mode,
+                    stage: dataFromServer.stage,
+                    is_notify: dataFromServer.is_notify,
+                    status: dataFromServer.status,
+                    current_location: dataFromServer.current_location,
+                    total_location_transfer: dataFromServer.total_location_transfer,
+                    done_location_transfer: dataFromServer.done_location_transfer,
+                    source: source,
+                    destination: destination
+                }
+            ];
+        } else {
+            payload = [dataFromServer];
+        }
     } else if (dataFromServer.mode === 5) {
-
+        payload = [dataFromServer];
     }
 
     return payload;
