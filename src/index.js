18aa6d41e5d5afa521aa4389f983c65d6c94e83a
Sorawis Pim-aksorn <68042695+tipleaum@users.noreply.github.com>
2021-04-15 22:49:18 +0700

    add ProviderNotification

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ProviderNotification from './context/Notification/ProviderNotification';
 import './css/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ProviderNotification>
+      <App />
+    </ProviderNotification>
   </React.StrictMode>,
   document.getElementById('root')
 );
