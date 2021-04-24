ac143d0172030b1b0766e46b2b0c0e63466e9462
featherflare <max43607@gmail.com>
2021-04-24 08:57:33 +0700

    fix css login

--- src/screen/LogIn.js
+++ src/screen/LogIn.js
@@ -5,11 +5,17 @@ import QrcodeLogIn from '../component/QrcodeLogIn';
 const Login = () => {
   const [username, setUserName] = useState('username');
   const [password, setPassword] = useState('password');
-  
+  const [qrCodeCam, setQrCodeCam] = useState(false);
+  const handleQRCode = () => {
+    if (!qrCodeCam) {
+      setQrCodeCam(true);
+    } else if (qrCodeCam) {
+      setQrCodeCam(false);
+    }
+  };
   return (
     <>
       <div className='container-login'>
-        <QrcodeLogIn />
         <div className='leftBox-login'>
           <div className='textbox'>
             <div className='text1'>SMART</div>
@@ -17,35 +23,47 @@ const Login = () => {
             <div className='text3'>PROJECT</div>
           </div>
         </div>
-        <div className='rightBox-login'>
-          <div className='text5'>SIGN IN</div>
-          <div>
-            <input
-              type='text'
-              name='username'
-              className='input-text'
-              placeholder='username'
-              onChange={(e) => setUserName(e.target.value)}
-            />
+        {!qrCodeCam && (
+          <div className='rightBox-login'>
+            <div className='text5'>SIGN IN</div>
+            <div>
+              <input
+                type='text'
+                name='username'
+                className='input-text'
+                placeholder='username'
+                onChange={(e) => setUserName(e.target.value)}
+              />
+            </div>
+            <div>
+              <input
+                type='password'
+                name='password'
+                className='input-text'
+                placeholder='password'
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div>
+              <button className='btn-login' type='submit'>
+                SIGN IN
+              </button>
+            </div>
+            <div>
+              <button className='btn-login' onClick={handleQRCode}>
+                QR code
+              </button>
+            </div>
           </div>
-          <div>
-            <input
-              type='password'
-              name='password'
-              className='input-text'
-              placeholder='password'
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div>
-            <button className='btn-login' type='submit'>
-              SIGN IN
+        )}
+        {qrCodeCam && (
+          <div className='rightBox-qrCode'>
+            <QrcodeLogIn />
+            <button className='btn-back' onClick={handleQRCode}>
+              back
             </button>
           </div>
-          <div>
-            <button className='btn-login'>QR code</button>
-          </div>
-        </div>
+        )}
       </div>
     </>
   );
